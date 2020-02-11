5cc9993a932060caacd595663c695e3465aed1f7
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-11 15:19:28 -0800

    trying to install font to my application

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+//components
+import Login from './components/Login/Login';
+import HackLogin from './components/HackLogin/HackingLogin';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
