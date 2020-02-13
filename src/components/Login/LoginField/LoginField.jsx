7b9dedbae8a766ce0c810c3c2c2b2614fcab5649
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-13 11:49:17 -0800

    Added in text based game

--- src/components/Login/LoginField/LoginField.jsx
+++ src/components/Login/LoginField/LoginField.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class LoginField extends Component {
     constructor(props) {
         super(props);
-        this.state = { password: '', tries: 0 };
+        this.state = { password: '', tries: 3 };
     }
 
     componentDidMount = () =>{
@@ -11,20 +11,24 @@ class LoginField extends Component {
     }
 
     onFinish = () => {
-        if (this.state === this.props.randomWord){
+        if (this.state === this.props.randomWord) {
             this.props.history.push('/USER')
         }
-        this.setState({ tries: this.state.tries + 1 })
+        this.setState({ tries: this.state.tries - 1 })
+        if (this.state.tries) {
+
+        }
     }
 
     render() {
         console.log(this.props.randomWord)
         return (
             <div>
+                {this.state.tries < 0 ? <h1>tries left :{this.state.tries}</h1>: ''}
                 <input type="text" value={this.state.password} onChange={(e)=> this.setState({
                     password: e.target.value
                 })}/>
-                <button onClick={this.onFinish()}> login</button>
+                <button onClick={this.onFinish()}>login</button>
             </div>
         );
     }
