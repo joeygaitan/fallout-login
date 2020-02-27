d7c3f8c8ad1a56ea0a6939ff6bdf00861b08c242
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-27 11:21:16 -0800

    figured out generation of random USC2 hexadecimal generation for the future rows

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,29 @@ import Login from './components/Login/Login';
 import HackLogin from './components/HackLogin/HackingLogin';
 import User from './components/UserPage/User';
 
+function randomUCS2HexaDecimalGenrator () {
+  let randomString = '0xF' + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+  return randomString
+}
+
+function rowGenerator () { 
+  let array = []
+  for (let i = 0; i <= 1; i += 1){
+    array.push({})
+    for(let j = 1; j <= 18; j += 1)
+    array[i][j] = randomUCS2HexaDecimalGenrator()
+  }
+  return array
+}
+
 const App = () => {
+
   let word = randomWords()
   let words = randomWords(7)
   let name = "ROOTUSER"
+  let generator = rowGenerator()
   
+  console.log(generator)
   return (
     <BrowserRouter>
       <div>
@@ -26,5 +44,9 @@ const App = () => {
   </BrowserRouter>
   );
 }
+
+
+
+
 
 export default App;
